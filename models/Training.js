947456cbf478ce8261182f9cd507f723aca272c4
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-02-16 12:56:36 +0100

    create training model

---
+++ models/Training.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const exerciseSchema = mongoose.Schema({
+	name: { type: String, required: false },
+	repeat: { type: Number, required: false },
+	time: { type: String, required: false },
+	weight: { type: Number, required: false },
+});
+
+const trainingSchema =  mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User',
+	},
+	exercise: {
+		type: [
+			{
+				type: [exerciseSchema],
+			},
+		],
+		required: true,
+	},
+});
+
+const Training = mongoose.model('Training', trainingSchema);
+
+export default Training;
