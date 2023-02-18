63c0cdc29ecbd9f43f459f99c076cd29050ab457
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-02-18 13:04:10 +0100

    change name req.id for req.user in protect middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
 			token = req.headers.authorization.split(' ')[1];
 			const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-			req.id = await User.findById(decoded.UserInfo.id).select('-password');
+			req.user = await User.findById(decoded.UserInfo.id).select('-password');
 
 			next();
 		} catch (error) {
